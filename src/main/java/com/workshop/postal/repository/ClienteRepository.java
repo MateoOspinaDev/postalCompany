32bf40e3681e9bdb2b0ad33dbfabe1803fd9dc06
MateoOspina <mateo.ospina@monolegal.co>
2023-10-21 13:47:11 -0500

    Se añaden anotaciones que faltaban

--- src/main/java/com/workshop/postal/repository/ClienteRepository.java
+++ src/main/java/com/workshop/postal/repository/ClienteRepository.java
@@ -2,6 +2,7 @@ package com.workshop.postal.repository;
 
 import com.workshop.postal.models.Cliente;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
--- src/main/java/com/workshop/postal/repository/EnvioRepository.java
+++ src/main/java/com/workshop/postal/repository/EnvioRepository.java
@@ -3,9 +3,11 @@ package com.workshop.postal.repository;
 import com.workshop.postal.models.Envio;
 import com.workshop.postal.models.enums.EstadoEnvio;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
+@Repository
 public interface EnvioRepository extends JpaRepository<Envio,String> {
     Envio findByNumeroGuia(String numeroGuia);
     Boolean existsByNumeroGuia(String numeroGuia);
