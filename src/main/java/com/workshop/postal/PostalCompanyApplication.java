49112d8dca2e1121140838dadf85f3b00276ff80
MateoOspina <mateo.ospina@monolegal.co>
2023-10-23 19:46:36 -0500

    prueba

--- src/main/java/com/workshop/postal/PostalCompanyApplication.java
+++ src/main/java/com/workshop/postal/PostalCompanyApplication.java
@@ -11,19 +11,7 @@ import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.security.SecurityRequirement;
 import io.swagger.v3.oas.models.security.SecurityScheme;
 
-@OpenAPIDefinition(
-		info = @Info(
-				title = "Servicio De Gestion De Mensajeria",
-				description = "Esta api es una api realizada como proyecto final para el bootcamp de makaia, busca automatizar y facilitar el proceso de gestion de los procesos que debe realizar una empresa de mensajeria," +
-						"Usando las mejores tecnologias, si deseas obtener mas informacion sobre el proyecto puedes visitar el proyecto en github--->  https://github.com/MateoOspinaDev/postalCompany"+
-						" O escribiendo a nuestros correos -->(mateoospinadesarrollo@gmail.com o smyardev2@gmail.com)",
-				version = "1.0",
-				contact = @Contact(name = "Mateo Ospina--Santiago Martinez", email = "smyardev2@gmail.com")
 
-
-
-		)
-)
 @SpringBootApplication
 public class PostalCompanyApplication {
 
@@ -31,14 +19,6 @@ public class PostalCompanyApplication {
 		SpringApplication.run(PostalCompanyApplication.class, args);
 	}
 
-	@Bean
-	public OpenAPI openAPI() {
-		return new OpenAPI()
-				.components(new Components()
-						.addSecuritySchemes("bearer", new SecurityScheme()
-								.type(SecurityScheme.Type.HTTP)
-								.scheme("bearer")))
-				.addSecurityItem(new SecurityRequirement().addList("bearer"));
-	}
+
 
 }
