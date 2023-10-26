e3df7e76e73129719397ebba298cb3d0e57ae3d2
MateoOspina <mateo.ospina@monolegal.co>
2023-10-25 19:01:26 -0500

    Se habilita CORS 2

--- src/main/java/com/workshop/postal/Security/filters/AuthenticationFilter.java
+++ src/main/java/com/workshop/postal/Security/filters/AuthenticationFilter.java
@@ -35,14 +35,12 @@ public class AuthenticationFilter extends OncePerRequestFilter {
         final String jwt;
         final String userEmail;
 
-        response.setHeader("Access-Control-Allow-Origin", "*");
-
-        response.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
-        response.setHeader("Access-Control-Allow-Headers", "authorization, content-type");
-
-        response.setHeader("Access-Control-Allow-Credentials", "true");
-
-        response.setHeader("Access-Control-Max-Age", "3600");
+        response.addHeader("Access-Control-Allow-Origin", "*");
+        response.addHeader("Access-Control-Allow-Methods", "GET, POST, DELETE, PUT, PATCH, HEAD");
+        response.addHeader("Access-Control-Allow-Headers", "Origin, Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers");
+        response.addHeader("Access-Control-Expose-Headers", "Access-Control-Allow-Origin, Access-Control-Allow-Credentials");
+        response.addHeader("Access-Control-Allow-Credentials", "true");
+        response.addIntHeader("Access-Control-Max-Age", 10);
 
         if (request.getMethod().equals("OPTIONS")) {
             response.setStatus(HttpServletResponse.SC_OK);
