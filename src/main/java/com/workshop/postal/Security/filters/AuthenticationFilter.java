4b888f88d0eedcad38ed902623bbf52d5363d9b0
MateoOspina <mateo.ospina@monolegal.co>
2023-10-25 19:20:04 -0500

    Se habilita CORS 3

--- src/main/java/com/workshop/postal/Security/filters/AuthenticationFilter.java
+++ src/main/java/com/workshop/postal/Security/filters/AuthenticationFilter.java
@@ -40,7 +40,7 @@ public class AuthenticationFilter extends OncePerRequestFilter {
         response.addHeader("Access-Control-Allow-Headers", "Origin, Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers");
         response.addHeader("Access-Control-Expose-Headers", "Access-Control-Allow-Origin, Access-Control-Allow-Credentials");
         response.addHeader("Access-Control-Allow-Credentials", "true");
-        response.addIntHeader("Access-Control-Max-Age", 10);
+        response.addIntHeader("Access-Control-Max-Age", 1000);
 
         if (request.getMethod().equals("OPTIONS")) {
             response.setStatus(HttpServletResponse.SC_OK);
